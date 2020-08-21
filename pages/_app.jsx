e2ac19c10fdb1e2b77e1c6613c0d7f2297a25a3b
Charles Bergeron <chuck.bergeron@gmail.com>
2020-08-21 14:31:32 -0700

    * fix the translation text not loading and add a loading indicator prior to translations bein ready

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,6 +8,7 @@ import { AllContextProviders } from 'lib/components/contextProviders/AllContextP
 // import { Chart } from 'lib/components/Chart'
 import { Layout } from 'lib/components/Layout'
 import { V3ApolloWrapper } from 'lib/components/V3ApolloWrapper'
+import { V3LoadingDots } from 'lib/components/V3LoadingDots'
 
 // import "@reach/dialog/styles.css";
 import "@reach/menu-button/styles.css"
@@ -26,7 +27,7 @@ import 'assets/styles/transitions.css'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-  // const [initialized, setInitialized] = useState(false)
+  const [initialized, setInitialized] = useState(false)
 
   useEffect(() => {
     Fathom.load('ESRNTJKP', {
@@ -57,22 +58,29 @@ function MyApp({ Component, pageProps }) {
     }
   }, [])
 
-  // useEffect(() => {
-  //   const initi18next = async () => {
-  //     await i18next.initPromise.then(() => {
-  //       setInitialized(true)
-  //     })
-  //   }
-  //   initi18next()
-  // }, [])
+  useEffect(() => {
+    const initi18next = async () => {
+      await i18next.initPromise.then(() => {
+        setInitialized(true)
+      })
+    }
+    initi18next()
+  }, [])
 
-  // if (!initialized) {
-  //   // return null
-  //   return <div
-  //     className='h-full w-full fixed t-0 r-0 l-0 b-0'
-  //     style={{ backgroundColor: '#1E0B43' }}
-  //   >LOADING</div> // could show loader ...
-  // }
+  if (!initialized) {
+    // return null
+    return <div
+      className='h-full w-full fixed t-0 r-0 l-0 b-0 text-white flex flex-col items-center justify-center'
+      style={{ backgroundColor: '#1E0B43', color: 'white' }}
+    >
+      <V3LoadingDots />
+      <h4
+        className='-mt-6'
+      >
+        Filling up the pools ...
+      </h4>
+    </div> // could show loader ...
+  }
   
   return <>
     {/* <Chart /> */}
