f53a450d698d1b619a6c30b07ae738f71eaeadaa
Charles Bergeron <chuck.bergeron@gmail.com>
2020-10-01 17:44:04 -0700

    * improve on 'suggest pool' row since we don't want the whole thing clickable

--- lib/components/PoolRowGhost.jsx
+++ lib/components/PoolRowGhost.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import FeatherIcon from 'feather-icons-react'
 
 import { useTranslation } from 'lib/../i18n'
 import { ButtonLink } from 'lib/components/ButtonLink'
-import { InteractableCard } from 'lib/components/InteractableCard'
+import { NonInteractableCard } from 'lib/components/NonInteractableCard'
 
 export const PoolRowGhost = (
   props,
@@ -11,13 +10,11 @@ export const PoolRowGhost = (
   const { t } = useTranslation()
   
   return <>
-    <InteractableCard
-      selected
-      key={`pool-row-ghost-card`}
-      href='https://discord.gg/hxPhPDW'
-      as={`https://discord.gg/hxPhPDW`}
+    <div
+      className='hover:bg-default border-accent-4 border-dashed border-2 w-full px-4 xs:px-6 mb-4 py-5 sm:py-6 trans rounded-lg text-inverse'
     >
-      <div className='flex flex-col items-center py-6'>
+
+      <div className='flex flex-col items-center py-6 opacity-70 hover:opacity-100 trans'>
         <h6
           className='flex justify-center font-bold w-full text-default-soft mb-4 text-center'
         >
@@ -33,6 +30,6 @@ export const PoolRowGhost = (
           {t('suggestPool')}
         </ButtonLink>
       </div>
-    </InteractableCard>
+    </div>
   </>
 }
