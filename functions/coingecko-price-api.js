364680d615f728059292bf7c1f3957103587fcaa
Charles Bergeron <chuck.bergeron@gmail.com>
2020-10-26 16:20:16 -0700

    * playing with coingecko api

--- functions/coingecko-price-api.js
+++ functions/coingecko-price-api.js
@@ -3,16 +3,17 @@ const request = require("request")
 exports.handler = (event, context, callback) => {
   request({
     method: 'GET',
-    url: `https://api.coingecko.com/api/v3/simple/price?ids=dai,usd-coin,tether&vs_currencies=eth,usd,cad`
+    url: `https://api.coingecko.com/api/v3/simple/price?ids=dai&vs_currencies=eth,usd,cad`
+    // url: `https://api.coingecko.com/api/v3/simple/price?ids=dai,usd-coin,tether&vs_currencies=eth,usd,cad`
   }, (error, response, body) => {
     if (error) {
-      // console.log(error)
-      // console.log(response)
-      // console.log(body)
+      console.log(error)
+      console.log(response)
+      console.log(body)
       callback(error, null)
     }
     console.log("Body: " + JSON.parse(body))
-    // console.log("Status Code: " + response.statusCode)
+    console.log("Status Code: " + response.statusCode)
 
     if (response.statusCode < 300) {
       callback(null, {
@@ -25,7 +26,7 @@ exports.handler = (event, context, callback) => {
         body
       })
     } else {
-      // console.log("Error", bodyObject)
+      console.log("Error", bodyObject)
       callback(bodyObject, {
         body: JSON.stringify({
           
