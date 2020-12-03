138ab8a7f088a99771177ae3aaab0ceeab5aae71
Charles Bergeron <chuck.bergeron@gmail.com>
2020-12-02 16:11:10 -0800

    * skip sentry release for time being

--- scripts/sentry-release.sh
+++ scripts/sentry-release.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Assumes you're in a git repository
-export SENTRY_ORG=pooltogether
+# export SENTRY_ORG=$SENTRY_ORG
 
-if [[ -z "$NEXT_JS_RELEASE_VERSION" ]]; then
-    echo "Must provide NEXT_JS_RELEASE_VERSION in environment" 1>&2
-    exit 1
-fi
+# if [[ -z "$NEXT_JS_RELEASE_VERSION" ]]; then
+#     echo "Must provide NEXT_JS_RELEASE_VERSION in environment" 1>&2
+#     exit 1
+# fi
 
-# Create a release
-sentry-cli releases new $NEXT_JS_RELEASE_VERSION
+# # Create a release
+# sentry-cli releases new $NEXT_JS_RELEASE_VERSION
 
-# Upload the source maps
-sentry-cli releases files $NEXT_JS_RELEASE_VERSION upload-sourcemaps out
+# # Upload the source maps
+# sentry-cli releases files $NEXT_JS_RELEASE_VERSION upload-sourcemaps out
 
-# Associate commits with the release
-sentry-cli releases set-commits $NEXT_JS_RELEASE_VERSION --commit "$SENTRY_REPO_NAME@$COMMIT_REF"
+# # Associate commits with the release
+# sentry-cli releases set-commits $NEXT_JS_RELEASE_VERSION --commit "$SENTRY_REPO_NAME@$COMMIT_REF"
 
-# Finalize the release
-sentry-cli releases finalize $NEXT_JS_RELEASE_VERSION
+# # Finalize the release
+# sentry-cli releases finalize $NEXT_JS_RELEASE_VERSION
