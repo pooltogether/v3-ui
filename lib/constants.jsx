1dcb98089d6e86f191786465437e425001691cdf
Aodhgan <aodhgan@pooltogether.com>
2021-01-25 16:14:15 -0800

    another test json

--- lib/constants.jsx
+++ lib/constants.jsx
@@ -108,7 +108,7 @@ export const CONTRACT_ADDRESSES = {
 
     // TODO: Add mainnet. Add remaining pools. Pull from pooltogether-contracts
     ComptrollerProxyFactory: '0xA9fbACfF0E0c574138d511eA333a0a2119AdB32D',
-    MerkleDistributor: '0xea266A665Ce1799f7eB21c629314Be2f28a02fde'
+    MerkleDistributor: '0x0c6271c65DA3183A2e7a0914c840a6F0a04D050d'
   }
 }
 
