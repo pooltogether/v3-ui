e07fa67b01544a1c5870a7e24d7998e9809cec51
Charles Bergeron <chuck.bergeron@gmail.com>
2021-01-30 10:15:45 -0800

    * quick fix for missing token images in lootbox tables

---
+++ functions/coingeckoImagesByContractAddress.js
@@ -0,0 +1,37 @@
+import { axiosInstance } from '../lib/axiosInstance'
+
+exports.handler = async (event, context, callback) => {
+  const { address } = event.queryStringParameters
+
+  const url = `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}`
+
+  try {
+    const response = await axiosInstance.get(url)
+
+    if (response && response.status === 404) {
+      return {
+        statusCode: 404,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ [address]: null })
+      }
+    } else if (response && response.status < 400) {
+      return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ [address]: { image: response.data.image } })
+      }
+    } else {
+      return {
+        statusCode: response.status,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'body': response.data })
+      }
+    }
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message
+    }
+  }
+
+}
