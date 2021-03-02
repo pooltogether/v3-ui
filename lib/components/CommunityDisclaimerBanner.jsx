8d99c08d96d0182d27879813e31226ff1f400665
Charles Bergeron <chuck.bergeron@gmail.com>
2021-03-02 12:56:09 -0800

    * starting on slight design tweaks

--- lib/components/CommunityDisclaimerBanner.jsx
+++ lib/components/CommunityDisclaimerBanner.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { Banner } from 'lib/components/Banner'
 import { Trans, useTranslation } from 'lib/../i18n'
 
-import Bell from 'assets/images/bell-red@2x.png'
+import Bell from 'assets/images/bell@2x.png'
+// import Bell from 'assets/images/bell-red@2x.png'
 
 export const CommunityDisclaimerBanner = (props) => {
   const { t } = useTranslation()
@@ -14,11 +15,11 @@ export const CommunityDisclaimerBanner = (props) => {
       className='bg-functional-red absolute t-0 l-0 r-0 mt-1 mb-8 flex flex-row items-center'
       style={{ minHeight: 150 }}
     >
-      <div className='mr-4 xs:mr-4 xs:ml-4 my-auto h-10'>
-        <img className='shake' src={Bell} style={{ maxWidth: 30 }} />
+      <div className='mr-4 xs:mr-4 xs:ml-4 my-auto h-12'>
+        <img className='shake' src={Bell} style={{ maxWidth: 40 }} />
       </div>
       
-      <h5 className='text-functional-red sm:leading-tight sm:ml-2'>
+      <h5 className='text-inverse sm:leading-tight sm:ml-2'>
         {t('anyoneCanCreateAPrizePool')}
         <br />
         <span className='font-normal'><Trans
@@ -27,7 +28,7 @@ export const CommunityDisclaimerBanner = (props) => {
           components={{
             a: <a
               target='_blank'
-              className='underline text-functional-red'
+              className='underline text-inverse'
               href='https://medium.com/pooltogether/prize-pool-builder-9f9c95fad860'
             />
           }}
