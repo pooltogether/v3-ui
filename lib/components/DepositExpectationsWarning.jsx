f24eacd035c49788c673d15042429785316321ba
Charles Bergeron <chuck.bergeron@gmail.com>
2021-03-01 16:44:54 -0800

    * only render the msg if we have the pool data * fix the trailing .0 on percent points

--- lib/components/DepositExpectationsWarning.jsx
+++ lib/components/DepositExpectationsWarning.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import { ethers } from 'ethers'
 
 import { SECONDS_PER_DAY } from 'lib/constants'
-import { Banner } from 'lib/components/Banner'
 import { Trans } from 'lib/../i18n'
+import { Banner } from 'lib/components/Banner'
+import { displayPercentage } from 'lib/utils/displayPercentage'
 
 import Bell from 'assets/images/bell-red@2x.png'
+
+const bn = ethers.utils.bigNumberify
 
 export const DepositExpectationsWarning = (props) => {
   const { pool } = props
   
   /// @return creditLimitMantissa The credit limit fraction
   //          This number is used to calculate both the credit limit and early exit fee.
-  const creditLimitMantissaBN = ethers.utils.bigNumberify(pool?.tokenCreditRates?.[0]?.creditLimitMantissa)
+  const creditLimitMantissa = pool?.tokenCreditRates?.[0]?.creditLimitMantissa
+  if (!creditLimitMantissa) {
+    return null
+  }
+
+  const creditLimitMantissaBN = creditLimitMantissa ? bn(creditLimitMantissa) : bn(0)
   const percent = ethers.utils.formatEther(creditLimitMantissaBN.mul(100)) || '-'
 
   /// @return creditRateMantissa The credit rate. This is the amount of tokens that accrue per second
-  const creditRateMantissaBN = ethers.utils.bigNumberify(pool?.tokenCreditRates?.[0]?.creditRateMantissa)
+  const creditRateMantissaBN = bn(pool?.tokenCreditRates?.[0]?.creditRateMantissa)
   const durationInSecondsBN = creditLimitMantissaBN.div(creditRateMantissaBN)
   const days = durationInSecondsBN.toNumber() / SECONDS_PER_DAY
 
@@ -41,7 +49,7 @@ export const DepositExpectationsWarning = (props) => {
           }}
           values={{
             days,
-            percent
+            percent: displayPercentage(percent)
           }}
         />
       </span>
