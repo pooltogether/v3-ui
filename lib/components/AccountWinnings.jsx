3943f342cecdddd5142184d10570047f8e1724d6
Charles Bergeron <chuck.bergeron@gmail.com>
2021-03-03 13:56:49 -0800

    * fix winnings from not showing properly at all

--- lib/components/AccountWinnings.jsx
+++ lib/components/AccountWinnings.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import Link from 'next/link'
+import { ethers } from 'ethers'
+import { isEmpty } from 'lodash'
 
 import { useTranslation } from 'lib/../i18n'
 import { AuthControllerContext } from 'lib/components/contextProviders/AuthControllerContextProvider'
-import { PoolNumber } from 'lib/components/PoolNumber'
+import { PoolCurrencyIcon } from 'lib/components/PoolCurrencyIcon'
 import { useContractAddresses } from 'lib/hooks/useContractAddresses'
 import { usePlayerPrizesQuery } from 'lib/hooks/usePlayerPrizesQuery'
 import { displayAmountInEther } from 'lib/utils/displayAmountInEther'
-import { sumAwardedControlledTokens } from 'lib/utils/sumAwardedControlledTokens'
 
 import IconTarget from 'assets/images/icon-target@2x.png'
 
@@ -26,10 +27,20 @@ export const AccountWinnings = () => {
 
   let awardedControlledTokens = prizesWon?.awardedControlledTokens || []
 
-  awardedControlledTokens = awardedControlledTokens.filter((awardedControlledToken) =>
-    contractAddresses.pools.includes(awardedControlledToken.prize.prizePool.id)
-  )
-  const total = sumAwardedControlledTokens(awardedControlledTokens)
+  const awarded = {}
+  awardedControlledTokens.forEach(awardedControlledToken => {
+    const pool = awardedControlledToken.prize.prizePool
+    const award = awarded[pool.id] || { total: ethers.utils.bigNumberify(0) }
+    const amount = ethers.utils.bigNumberify(awardedControlledToken.amount)
+
+    award.ticker = pool.underlyingCollateralSymbol
+    award.decimals = pool.underlyingCollateralDecimals
+    award.total = award.total.gt(0) ? award.total.add(amount) : amount
+
+    awarded[pool.id] = award
+  })
+
+  const awardKeys = !isEmpty(awarded) ? Object.keys(awarded) : []
 
   // TODO: We should calculate all of the ERC20s someone won, their value on the day it was awarded
   // as well as the interest prizes!
@@ -39,32 +50,45 @@ export const AccountWinnings = () => {
       <h5 className='font-normal text-accent-2 mt-16 mb-4'>{t('myWinnings')}</h5>
 
       <div className='xs:mt-3 bg-accent-grey-4 rounded-lg xs:mx-0 px-2 sm:px-6 py-3'>
-        <div className='flex justify-between flex-col xs:flex-row xs:pt-4 pb-0 px-2 xs:px-4'>
-          <div className='flex-col order-2 xs:order-1'>
-            <h6 className='flex items-center font-normal'>{t('allTimeWinnings')}</h6>
+        <div className='flex justify-between xs:py-4 pb-0 px-2 xs:px-4'>
 
-            <h3>
-              $<PoolNumber>{displayAmountInEther(total, { precision: 2 })}</PoolNumber>
-            </h3>
+          <div className='w-1/2'>
+            <div className='mb-10 font-bold'>
+              {t('youHaveNotWonYet')}
+              <br />{t('keepYourDepositsInThePoolsToWin')}
+            </div>
+
+            <table className='table-fixed text-xxs xs:text-base sm:text-xl w-full'>
+              <tbody>
+                {awardKeys.map(awardKey => {
+                  const award = awarded[awardKey]
+
+                  return <tr key={`award-winnings-row-${awardKey}`}>
+                    <td className='px-2 sm:px-3 text-left font-bold'>
+                      <span className='mr-2'>
+                        <PoolCurrencyIcon sm pool={{ underlyingCollateralSymbol: award.ticker }} />
+                      </span>
+                      {displayAmountInEther(award.total, {
+                        precision: 2,
+                        decimals: award.decimals
+                      })} {award.ticker}
+                    </td>
+                  </tr>
+                })}
+              </tbody>
+            </table>
+            {/* TODO: Bring back the USD value of all winnings, when they won them and with all external tokens and lootbox tokens */}
+            {/* <h6 className='flex items-center font-normal'>{t('allTimeWinnings')}</h6> */}
+            {/* <h3> */}
+              {/* $<PoolNumber>{displayAmountInEther(total, { precision: 2 })}</PoolNumber> */}
+            {/* </h3> */}
           </div>
 
-          <div className='order-1 xs:order-2 ml-auto'>
+          <div className='ml-auto'>
             <img src={IconTarget} className='w-24 h-24 mx-auto' />
           </div>
         </div>
 
-        <div className='text-inverse flex flex-col justify-between xs:px-2'>
-          <table className='table-fixed w-full text-xxs xs:text-base sm:text-xl mt-6'>
-            <tbody></tbody>
-          </table>
-        </div>
-
-        <div className='text-xxxs xs:text-xxs sm:text-xs opacity-70 px-2 xs:px-4 py-4'>
-          {t('seeAllPastWinnersOn')}{' '}
-          <Link href='/prizes/PT-cDAI' shallow>
-            <a className='text-accent-grey-1 underline'>{t('pastWinnersPrizeHistoryLink')}</a>
-          </Link>
-        </div>
       </div>
     </>
   )
