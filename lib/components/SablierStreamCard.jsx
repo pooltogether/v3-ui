0599c59c30883c853288ba65e898926bca14de2d
Charles Bergeron <chuck.bergeron@gmail.com>
2021-03-31 12:07:08 -0700

    * differentiate sablier content

--- lib/components/SablierStreamCard.jsx
+++ lib/components/SablierStreamCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import classnames from 'classnames'
-import { Trans, useTranslation } from 'lib/../i18n'
-
-import SablierSvg from 'assets/images/sablier.svg'
-import { getMaxPrecision, numberWithCommas } from 'lib/utils/numberWithCommas'
-import { secondsSinceEpoch } from 'lib/utils/secondsSinceEpoch'
 import { ethers } from 'ethers'
-import { getDateFromSeconds } from 'lib/utils/getDateFromSeconds'
+import { useTranslation } from 'lib/../i18n'
+
+import { PoolNumber } from 'lib/components/PoolNumber'
 import { Erc20Image } from 'lib/components/Erc20Image'
 import { EtherscanAddressLink } from 'lib/components/EtherscanAddressLink'
+import { getDateFromSeconds } from 'lib/utils/getDateFromSeconds'
+import { numberWithCommas } from 'lib/utils/numberWithCommas'
+import { secondsSinceEpoch } from 'lib/utils/secondsSinceEpoch'
 import { shorten } from 'lib/utils/shorten'
 
 export const SablierStreamCard = (props) => {
@@ -32,20 +32,23 @@ export const SablierStreamCard = (props) => {
   const streamTotalTime = stopTime.sub(startTime)
   const currentTimeRelativeToStreamStart = currentTime.sub(startTime)
   const percentOfStreamDone = currentTimeRelativeToStreamStart.mul(100).div(streamTotalTime)
+  console.log(streamTotalTime.toString())
+  console.log(prizePeriodSeconds)
   const prizesToBeStreamedTo = streamTotalTime.div(prizePeriodSeconds)
+  console.log(prizesToBeStreamedTo.toString())
 
   return (
     <div
       id='sablier-card'
       className='non-interactable-card my-4 py-4 xs:py-6 px-4 xs:px-6 sm:px-10 bg-card rounded-lg card-min-height-desktop'
     >
-      <h3 className='mb-4'>{t('sablierStream')}</h3>
+      <h3 className=''>{t('prizeStream')}</h3>
 
-      <div className='flex'>
-        <Erc20Image address={tokenAddress} className='my-auto' />
-        <h3>{tokenName}</h3>
+      <div className='flex items-center'>
+        <Erc20Image address={tokenAddress} />
+        <h6>{tokenName}</h6>
         <EtherscanAddressLink
-          className='text-accent-1 trans hover:text-inverse ml-4 mt-auto mb-2'
+          className='text-accent-1 trans hover:text-inverse ml-4'
           address={tokenAddress}
         >
           ({shorten(tokenAddress)})
@@ -53,29 +56,23 @@ export const SablierStreamCard = (props) => {
       </div>
 
       <div className='flex flex-col xs:flex-row justify-between mt-6'>
-        <div className='flex mb-2 xs:mb-0'>
-          <h3 className='leading-none'>
-            {numberWithCommas(amountPerPrizePeriod, {
-              precision: getMaxPrecision(amountPerPrizePeriod)
-            })}
-          </h3>
-          <span className='ml-2 mt-auto'>{t('tokenEveryPrize', { token: tokenSymbol })}</span>
+        <div className='flex items-baseline mb-2 xs:mb-0'>
+          <h5>
+            <PoolNumber>{numberWithCommas(amountPerPrizePeriod)}</PoolNumber>
+          </h5>
+          <span className='ml-2'>{t('tokenEveryPrize', { token: tokenSymbol })}</span>
         </div>
 
-        <div className='flex mb-2 xs:mb-0'>
-          <h3 className='leading-none'>
-            {numberWithCommas(totalDeposit, {
-              precision: getMaxPrecision(totalDeposit)
-            })}
-          </h3>
-          <span className='ml-2 mt-auto'>{t('tokenInTotal', { token: tokenSymbol })}</span>
+        <div className='flex items-baseline mb-2 xs:mb-0'>
+          <h5>
+            <PoolNumber>{numberWithCommas(totalDeposit)}</PoolNumber>
+          </h5>
+          <span className='ml-2'>{t('tokenInTotal', { token: tokenSymbol })}</span>
         </div>
 
-        <div className='flex'>
-          <h3 className='leading-none'>
-            {numberWithCommas(prizesToBeStreamedTo, { precision: 0 })}
-          </h3>
-          <span className='ml-2 mt-auto lowercase'>{t('prizes')}</span>
+        <div className='flex items-baseline'>
+          <h5>{prizesToBeStreamedTo.toString()}</h5>
+          <span className='ml-2 lowercase'>{t('prizes')}</span>
         </div>
       </div>
 
