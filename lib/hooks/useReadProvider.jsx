ed9da5f6227dd2211e917f170ded4a974e6ff3c1
Charles Bergeron <chuck.bergeron@gmail.com>
2021-04-19 19:45:10 -0700

    * cleanup

--- lib/hooks/useReadProvider.jsx
+++ lib/hooks/useReadProvider.jsx
@@ -6,12 +6,10 @@ import { useNetwork } from 'lib/hooks/useNetwork'
 
 export const useReadProvider = () => {
   const { chainId } = useNetwork()
-  // console.log(chainId)
 
   const { data: _readProvider, isFetched } = useQuery([QUERY_KEYS.readProvider, chainId], () =>
     readProvider(chainId)
   )
-  // console.log({ _readProvider })
 
   const isLoaded =
     _readProvider &&
@@ -25,29 +23,3 @@ export const useReadProvider = () => {
     isLoaded
   }
 }
-
-// import { useContext, useEffect, useState } from 'react'
-
-// import { AuthControllerContext } from 'lib/components/contextProviders/AuthControllerContextProvider'
-// import { readProvider } from 'lib/services/readProvider'
-
-// export function useReadProvider() {
-//   const { networkName } = useContext(AuthControllerContext)
-
-//   const [defaultReadProvider, setDefaultReadProvider] = useState({})
-
-//   useEffect(() => {
-//     const getReadProvider = async () => {
-//       if (networkName !== 'unknown network') {
-//         const defaultReadProvider = await readProvider(networkName)
-//         setDefaultReadProvider(defaultReadProvider)
-//       }
-//     }
-//     getReadProvider()
-//   }, [networkName])
-
-//   return {
-//     readProvider: defaultReadProvider,
-//     isLoaded: defaultReadProvider && Object.keys(defaultReadProvider).length > 0
-//   }
-// }
