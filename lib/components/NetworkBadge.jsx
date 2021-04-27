919a4c9de1483b66346450badea6574ad0847c87
Charles Bergeron <chuck.bergeron@gmail.com>
2021-04-27 13:01:31 -0700

    * fix account ticket on mobile

--- lib/components/NetworkBadge.jsx
+++ lib/components/NetworkBadge.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import { NetworkIcon } from 'lib/components/NetworkIcon'
 import { getNetworkNiceNameByChainId } from 'lib/utils/networks'
 
 export const NetworkBadge = (props) => {
   const { chainId, vertical } = props
-  let { sizeClasses, textClasses } = props
+  let { className, sizeClasses, textClasses } = props
 
+  const defaultClasses = 'mx-auto'
   textClasses = textClasses ?? 'text-xxs'
   sizeClasses = sizeClasses ?? 'w-4 h-4'
+  className = className ?? defaultClasses
 
   return (
-    <div className={`mx-auto flex items-center ${vertical ? 'mt-2 flex-col' : ''}`}>
+    <div className={classnames(className, `flex items-center ${vertical ? 'mt-2 flex-col' : ''}`)}>
       <NetworkIcon noMargin sizeClasses={sizeClasses} chainId={chainId} />
 
       <span className={`text-accent-1 capitalize ${textClasses} ${vertical ? 'my-1' : 'ml-1'}`}>
