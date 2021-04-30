4cb87e0ef3b9c97e3b2f321df7afd4c9b67a9150
Dylan DesRosier <dylan@pooltogether.com>
2021-04-30 14:58:15 -0400

    remove some hardcoding

--- lib/components/PoolPrizeCard.jsx
+++ lib/components/PoolPrizeCard.jsx
@@ -60,11 +60,6 @@ export const PoolPrizeCard = (props) => {
 const PrizeValue = (props) => {
   const { pool } = props
 
-  // Obviously move this to the API for all known Aave yield source pools and get the APR from Aave:
-  if (pool.prizePool.address === '0x887e17d791dcb44bfdda3023d26f7a04ca9c7ef4') {
-    return <USDPrizeValue amount={hardcodedAprAmountUsd(pool)} />
-  }
-
   if (!pool || !pool.prize?.totalValueUsd) {
     return (
       <PrizeValueHeader>
