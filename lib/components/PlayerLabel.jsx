83f57842e00aeba7ad5ed7150217e3c6a841ca20
Charles Bergeron <chuck.bergeron@gmail.com>
2021-05-26 12:47:08 -0700

    * don't inherit some parent styles for tooltips * add yearn usdc contract

--- lib/components/PlayerLabel.jsx
+++ lib/components/PlayerLabel.jsx
@@ -4,16 +4,19 @@ import { Tooltip } from 'lib/components/Tooltip'
 import { shorten } from 'lib/utils/shorten'
 
 const PLAYER_LABELS = {
-  '0x57e848a6915455a7e77cf0d55a1474befd9c374d': 'yearn.finance Users'
+  '0x57e848a6915455a7e77cf0d55a1474befd9c374d': 'yearn.finance DAI Users',
+  '0x387fca8d7e2e09655b4f49548607b55c0580fc63': 'yearn.finance USDC Users'
 }
 
 // mapping of YEARN strategy contracts to their corresponding vaults
 const YEARN_STRATEGIES_TO_VAULTS = {
-  '0x57e848a6915455a7e77cf0d55a1474befd9c374d': '0x19d3364a399d251e894ac732651be8b0e4e85001'
+  '0x57e848a6915455a7e77cf0d55a1474befd9c374d': '0x19d3364a399d251e894ac732651be8b0e4e85001',
+  '0x387fca8d7e2e09655b4f49548607b55c0580fc63': '0x5f18c75abdae578b483e5f43f12a39cf75b973a9'
 }
 
 const YEARN_STRATEGIES_TO_NUM_HOLDERS = {
-  '0x57e848a6915455a7e77cf0d55a1474befd9c374d': 939
+  '0x57e848a6915455a7e77cf0d55a1474befd9c374d': 939,
+  '0x387fca8d7e2e09655b4f49548607b55c0580fc63': 2240
 }
 
 export const PlayerLabel = (props) => {
