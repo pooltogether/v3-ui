d999c7b3e915e6cb56ea795aac551cfe723c6cca
Charles Bergeron <chuck.bergeron@gmail.com>
2021-06-30 13:43:06 -0700

    * fix layout jumping bug

--- lib/components/Layout/SideNav.jsx
+++ lib/components/Layout/SideNav.jsx
@@ -23,7 +23,7 @@ export const SideNav = (props) => {
 
   return (
     <div className='flex flex-col justify-between h-full'>
-      <SideNavContainer>
+      <SideNavContainer className='top-side-nav'>
         <SideNavLink
           href='/'
           as='/'
