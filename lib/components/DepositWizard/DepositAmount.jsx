80db2c55fddf851509b46a0353f593e570e4e4f8
Dylan DesRosier <dylan@pooltogether.com>
2021-08-05 15:08:24 -0400

    Copy for when quantity is too high

--- lib/components/DepositWizard/DepositAmount.jsx
+++ lib/components/DepositWizard/DepositAmount.jsx
@@ -58,7 +58,8 @@ export const DepositAmount = (props) => {
       if (usersAddress) {
         if (!usersUnderlyingBalance) return false
         if (!usersTicketBalance) return false
-        if (parseUnits(usersUnderlyingBalance, decimals).lt(parseUnits(v, decimals))) return false
+        if (parseUnits(usersUnderlyingBalance, decimals).lt(parseUnits(v, decimals)))
+          return t('insufficientFunds')
       }
       if (getMaxPrecision(v) > decimals) return false
       if (parseUnits(v, decimals).isZero()) return false
