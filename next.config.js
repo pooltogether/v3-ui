e39ddb78d050b459cf5178a0c9e206a846fd96e2
Charles Bergeron <chuck.bergeron@gmail.com>
2021-09-03 12:10:05 -0700

    * changes to next config for fleek

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const chalk = require("chalk")
+const chalk = require('chalk')
 const withImages = require('next-images')
 const webpack = require('webpack')
 const _ = require('lodash')
@@ -6,7 +6,7 @@ const _ = require('lodash')
 const isProduction = process.env.NODE_ENV === 'production'
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true'
 })
 
 const nextConfig = {
@@ -17,48 +17,45 @@ const nextConfig = {
   },
   compress: false,
   inlineImageLimit: 48, // make it tiny so that it doesn't inline,
-  // exportTrailingSlash: true,
-  // exportPathMap: function() {
-  //   return {
-  //     '/': { page: '/' }
-  //   };
-  // }
+  exportTrailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
+  }
 }
 
+const allConfig = withBundleAnalyzer(
+  withImages({
+    ...nextConfig,
+    async redirects() {
+      return [
+        {
+          source: '/prizes',
+          destination: '/prizes/mainnet/PT-cDAI',
+          permanent: true
+        }
+      ]
+    },
+    publicRuntimeConfig: {
+      locizeProjectId: process.env.NEXT_JS_LOCIZE_PROJECT_ID,
+      locizeApiKey: process.env.NEXT_JS_LOCIZE_DEV_API_KEY,
+      locizeVersion: process.env.NEXT_JS_LOCIZE_VERSION
+    },
+    webpack(config, options) {
+      // config.optimization.minimizer = []
 
-const allConfig =
-  withBundleAnalyzer(
-  withImages(
-    {
-      ...nextConfig,
-      async redirects() {
-        return [
-          {
-            source: '/prizes',
-            destination: '/prizes/mainnet/PT-cDAI',
-            permanent: true,
-          }
-        ]
-      },
-      publicRuntimeConfig: {
-        locizeProjectId: process.env.NEXT_JS_LOCIZE_PROJECT_ID,
-        locizeApiKey: process.env.NEXT_JS_LOCIZE_DEV_API_KEY,
-        locizeVersion: process.env.NEXT_JS_LOCIZE_VERSION
-      },
-      webpack(config, options) {
-        // config.optimization.minimizer = []
+      config.mode = isProduction ? 'production' : 'development'
+      config.devtool = isProduction ? 'hidden-source-map' : 'eval-source-map'
 
-        config.mode = isProduction ? 'production' : 'development'
-        config.devtool = isProduction ? 'hidden-source-map' : 'eval-source-map'
+      var appVars = _.keys(process.env).filter((key) => key.startsWith('NEXT_JS_'))
 
-        var appVars = _.keys(process.env).filter(key => key.startsWith('NEXT_JS_'))
+      config.plugins.push(new webpack.EnvironmentPlugin(_.pick(process.env, appVars)))
 
-        config.plugins.push(new webpack.EnvironmentPlugin(_.pick(process.env, appVars)))
-
-        return config
-      }
+      return config
     }
-  ))
+  })
+)
 
 console.log('')
 console.log(chalk.green('Using next.js config options:'))
