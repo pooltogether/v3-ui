c913d7cf022412dc3c69800637654109fb595010
Dylan DesRosier <dylan@pooltogether.com>
2021-09-07 17:49:39 -0400

    show underlying balance not shares. Fix withdrawals

--- lib/components/DepositWizard/Pods/PodReviewAndSubmitDeposit.jsx
+++ lib/components/DepositWizard/Pods/PodReviewAndSubmitDeposit.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Card, poolToast } from '@pooltogether/react-components'
-import { useUsersAddress, useSendTransaction } from '@pooltogether/hooks'
+import { useUsersAddress, useSendTransaction, usePodShareBalance } from '@pooltogether/hooks'
 import { ethers } from 'ethers'
-import { numberWithCommas } from '@pooltogether/utilities'
+import { numberWithCommas, underlyingAmountToSharesAmount } from '@pooltogether/utilities'
 import { useTranslation } from 'react-i18next'
 
 import { ReviewAndSubmitDeposit } from 'lib/components/DepositWizard/ReviewAndSubmitDeposit'
 import PodAbi from 'abis/PodAbi'
 import Bell from 'assets/images/bell-red@2x.png'
+import { useAllUsersPodTickets } from 'lib/hooks/useAllUsersPodTickets'
 
 export const PodReviewAndSubmitDeposit = (props) => {
   const { pod, contractAddress, quantity, nextStep } = props
+  const chainId = pod.metadata.chainId
   const tokenSymbol = pod.tokens.underlyingToken.symbol
+  const decimals = pod.tokens.underlyingToken.decimals
+  const podAddress = pod.pod.address
   const usersAddress = useUsersAddress()
   const { t } = useTranslation()
   const sendTx = useSendTransaction(t, poolToast)
 
-  const submitDepositTransaction = async () => {
-    const decimals = pod.tokens.underlyingToken.decimals
+  const { refetch: refetchPodShareBalance } = usePodShareBalance(chainId, usersAddress, podAddress)
+  const { refetch: refetchAllPodTickets } = useAllUsersPodTickets(usersAddress)
+
+  const submitDepositTransaction = useCallback(async () => {
     const quantityBN = ethers.utils.parseUnits(quantity, decimals)
 
     const params = [usersAddress, quantityBN]
@@ -32,10 +38,14 @@ export const PodReviewAndSubmitDeposit = (props) => {
       callbacks: {
         onSuccess: () => {
           nextStep()
+        },
+        refetch: () => {
+          refetchAllPodTickets()
+          refetchPodShareBalance()
         }
       }
     })
-  }
+  }, [quantity, contractAddress])
 
   return (
     <ReviewAndSubmitDeposit
